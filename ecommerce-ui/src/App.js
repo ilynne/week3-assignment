f465826443eb6c3fe8e6974a0797f87033a408d8
Lynne Cooney <ilynne@gmail.com>
2020-04-21 23:43:48 -0700

    adds polaroid style to image

--- ecommerce-ui/src/App.js
+++ ecommerce-ui/src/App.js
@@ -4,21 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'main-content'}>
+      <div className={'polaroid'}>
+        <img src={'https://images.unsplash.com/photo-1490806230066-f7e6f7bf5052?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2ca889535ea01f912f94ac4ddf0034e0&auto=format&fit=crop&w=500&q=80'} />
+        <span class={'caption'}>Denver USA</span>
+      </div>
     </div>
   );
 }
