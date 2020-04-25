a9dace96f9ed5bc3d278e6e6025fcda26d58c218
Lynne Cooney <ilynne@gmail.com>
2020-04-25 07:59:21 -0700

    adds css for polaroid image display

--- ecommerce-ui/src/App.js
+++ ecommerce-ui/src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className={'main-content'}>
-      <div className={'polaroid'}>
+      <div className={'polaroid-container'}>
         <img src={'https://images.unsplash.com/photo-1490806230066-f7e6f7bf5052?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2ca889535ea01f912f94ac4ddf0034e0&auto=format&fit=crop&w=500&q=80'} />
-        <p class={'caption'}>New York USA</p>
+        <p className={'caption'}>New York USA</p>
       </div>
     </div>
   );
